d7b83ddf71b4b48a2ff58023511a49fdea99854b
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2022-03-09 19:26:56 +0100

    keep

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ModeNightIcon from '@mui/icons-material/ModeNight';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+
+import { MyHeader, Title } from './styles'
+import { useTheme, useUpdateTheme } from '../ThemeContext/ThemeContext';
+
+export default function Header() {
+    const darkTheme = useTheme();
+    const toggleTheme = useUpdateTheme();
+
+    const theme = {
+        backgroundColor: darkTheme ? "#202124" : "#f5ba13",
+        border: darkTheme && "Solid 1px #eee"
+    }
+
+  return (
+    <div>
+        <MyHeader style={theme}>
+            <Title>Keep</Title>
+            <div onClick={toggleTheme}>
+                {darkTheme ? (
+                    <Brightness4Icon fontSize='large' sx={{color:"white"}}/>
+                ) : (
+                    <ModeNightIcon fontSize='large' sx={{color:"white"}}/>
+                )}
+            </div>
+            {/* sx para colores propios */}
+        </MyHeader>
+    </div>
+  )
+}
